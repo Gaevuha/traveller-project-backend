3b9987e9ac1365d318e6eece4c393db30c1e3b14
Serhii Haievoi <gaevuha@gmail.com>
2025-11-06 21:06:51 +0200

    fix auth and allUsers

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { getEnvVar } from './utils/getEnvVar.js';
 import router from './routers/index.js';
-import authRouter from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
--- src/services/users.js
+++ src/services/users.js
@@ -5,25 +5,20 @@ export const getAllUsers = async ({ page = 1, perPage = 12 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  try {
-    // Підрахунок кількості користувачів
-    const usersCount = await UsersCollection.countDocuments();
+  // Підрахунок кількості користувачів
+  const usersCount = await UsersCollection.countDocuments();
 
-    // Отримуємо користувачів без пароля
-    const users = await UsersCollection.find({}, '-password')
-      .skip(skip)
-      .limit(limit)
-      .lean();
+  // Отримуємо користувачів без пароля
+  const users = await UsersCollection.find({}, '-password')
+    .skip(skip)
+    .limit(limit)
+    .lean();
 
-    // Формуємо дані пагінації
-    const paginationData = calculatePaginationData(usersCount, perPage, page);
+  // Формуємо дані пагінації
+  const paginationData = calculatePaginationData(usersCount, perPage, page);
 
-    return {
-      data: users,
-      ...paginationData,
-    };
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    throw error;
-  }
+  return {
+    data: users,
+    ...paginationData,
+  };
 };
