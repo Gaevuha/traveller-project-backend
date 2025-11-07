dd29f8067d428d445454b7f5001ecd3f4c7a638e
kapustnyak <zned003@gmail.com>
2025-11-07 12:57:44 +0100

    Delete Story fix and comments

--- src/routers/users.js
+++ src/routers/users.js
@@ -23,7 +23,7 @@ router.get('/:userId', ctrlWrapper(getUsersByIdController)); // створити
 router.get('/me/profile', authenticate, ctrlWrapper(getMeProfileController)); // створити приватний ендпоінт на отримання інформації про поточного користувача
 router.post('/me/saved/:storyId', ctrlWrapper(createMeSavedStoriesController)); // створити приватний ендпоінт для додавання статті до збережених статей користувача
 
-router.delete('/me/saved/:storyId', ctrlWrapper(deleteMeSavedStoriesController)); // створити приватний ендпоінт для видалення статті зі збережених статей користувача
+router.delete('/me/saved/:storyId', authenticate, ctrlWrapper(deleteMeSavedStoriesController)); // створити приватний ендпоінт для видалення статті зі збережених статей користувача
 router.patch('/me/avatar', uploadAvatar, ctrlWrapper(patchMeAvatarController)); // створити приватний ендпоінт для оновлення аватару корситувача
 router.patch('/me', ctrlWrapper(patchMeController)); //створити приватний ендпоінт для оновлення даних користувача
 
